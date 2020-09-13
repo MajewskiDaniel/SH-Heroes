9c9418652de04f82192b3ac5348e541a5eb56221
AgnieszkaJarosik <jarosikloj@gmail.com>
2020-09-13 17:09:37 +0200

    [fix] fixed displaing tags, added types

--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -1,6 +1,5 @@
-import React, {useState} from "react";
-import { Input } from 'antd';
-import { Tag } from 'antd';
+import React, { useState, useRef} from "react";
+import { Input, Tag } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import {FieldInputProps} from "formik";
 
@@ -8,22 +7,21 @@ export interface ITags extends FieldInputProps<string[]>{
 }
 
 export const Tags: React.FC<ITags> = ({onChange, value, name}) => {
-
   const [inputVisible,  setInputVisible] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const showInput = async () => {
+    await setInputVisible(true);
+    const node = inputRef.current;
+    if(node){
+      node.focus();
+    }
+  }
 
   const handleClose = (removedTag: string) => {
     const newTags = value.filter(tag => tag !== removedTag);
-
     onChange({ target: { name, value: newTags }});
-  };
-
-  const showInput = () => {
-    setInputVisible(true);
-  };
-
-  const handleInputChange = (e: any) => {
-    setInputValue(e.target.value);
   };
 
   const  handleInputConfirm = () => {
@@ -32,54 +30,38 @@ export const Tags: React.FC<ITags> = ({onChange, value, name}) => {
       newTags = [...value, inputValue];
     }
     onChange({ target: { name, value: newTags }});
-
     setInputValue('');
     setInputVisible(false);
   };
 
-  const forMap = (tag: any) => {
-    return (
-      <span key={tag} style={{ display: 'inline-block' }}>
-         <Tag
-           closable
-           color="cyan"
-           onClose={(e: any) => {
-             e.preventDefault();
-             handleClose(tag);
-           }}
-         >
-        # {tag}
-      </Tag>
-      </span>
-    );
-  };
-
-  const tagChild = value.map(forMap);
-
-    return (
-      <>
-        <div style={{ margin: 16 }}>
-          <div>
-            {tagChild}
-          </div>
-        </div>
-        {inputVisible && (
-          <Input
-            // ref={inputEl}
-            type="text"
-            size="small"
-            style={{ width: 78 }}
-            value={inputValue}
-            onChange={handleInputChange}
-            onBlur={handleInputConfirm}
-            onPressEnter={handleInputConfirm}
-          />
-        )}
-        {!inputVisible && (
-          <Tag onClick={showInput} className="site-tag-plus">
-            <PlusOutlined /> New Tag
-          </Tag>
-        )}
+  return (
+    <>
+      <div style={{ margin: 16 }}>
+        { value.map( (tag: string) => (
+          <span key={tag} style={{ display: 'inline-block' }}>
+            <Tag closable color="blue"
+                 onClose={(e: React.ChangeEvent<HTMLInputElement>) => handleClose(tag)} >
+              # {tag}
+            </Tag>
+          </span>
+        ))}
+      </div>
+      {inputVisible && (
+        <Input
+          ref={inputRef as React.RefObject<any>}
+          type="text"
+          size="small"
+          style={{ width: 78 }}
+          value={inputValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm} />
+          )}
+      {!inputVisible && (
+        <Tag onClick={showInput} className="site-tag-plus">
+          <PlusOutlined /> New Tag
+        </Tag>
+      )}
       </>
-    );
+  );
 }
