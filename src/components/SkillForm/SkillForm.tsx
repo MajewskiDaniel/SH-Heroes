fd47d05aef125f175ffde0b2cc9b9ec7f342ee43
AgnieszkaJarosik <jarosikloj@gmail.com>
2020-09-15 08:15:41 +0200

    [wip] changed SkillForm

--- src/components/SkillForm/SkillForm.tsx
+++ src/components/SkillForm/SkillForm.tsx
@@ -1,30 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import {Field, FieldProps, Form, Formik, FormikHelpers, FormikProps} from 'formik';
 import {DatePicker, FormItem, Input, Select, SubmitButton} from 'formik-antd';
-import { notification, Divider } from 'antd';
+import { notification, Divider, Input as InputAnt } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { useHistory } from "react-router-dom";
 import * as yup from 'yup';
 
-import {Tags} from '../Tags/Tags';
-import {PhotoPicker} from "../PhotoPicker/PhotoPicker";
-import {EmployeesSvc} from "../../services/EmployeesSvc";
-import {EmployeePosition, employeePositionMap, IEmployee, SeniorityLevel, seniorityMap} from "../../models/employee";
+import { ISkill,  SkillWeight, skillWeightMap} from "../../models/employee";
 import styles from './SkillForm.module.scss';
 import {FormikErrors, FormikState} from "formik/dist/types";
-import {IAddForm} from "../AddForm/AddForm";
 
 const SkillSchema = yup.object().shape({
-  name: yup.string().required("Required"),
-  category: yup.array().required("Required"),
-  skillWeight: yup.array().required("Required"),
+  skillName: yup.string().required("Required"),
+  skillCategory: yup.string().required("Required"),
+  skillWeight: yup.string().required("Required"),
 });
 
 const INITIAL_VALUES = {
-  name: "",
-  category: [],
+  skillName: "",
+  skillCategory: [""],
   skillWeight: "",
 };
+
+type OmitAB = Omit<ISkill, "skillCategory"|"skillWeight">;
+
+interface IFormSkill extends OmitAB {
+  skillCategory: string[],
+  skillWeight: SkillWeight | string
+}
 
 export interface ISkillForm {
   id: string
@@ -32,22 +35,34 @@ export interface ISkillForm {
 
 export const SkillForm: React.FC<ISkillForm> = ({id}) => {
   const [submitted, setSubmitted] = useState<boolean>(false);
-  const [initialValuesWithId, setInitialValuesWithId] = useState(INITIAL_VALUES);
-  const [categories, setCategories] = useState([]);
+  const [initialValuesWithId, setInitialValuesWithId] = useState<IFormSkill>(INITIAL_VALUES);
+  const [categories, setCategories] = useState<string[]>([]);
+  const [newCategory, setNewCategory] = useState("");
   const history = useHistory();
 
-  const onInputAddCategoryChange = () => {
+  const fetchCategories = async () => {
+    setCategories(["HSE", "Finance", "Logistics"]);
+  }
 
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const onInputAddCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewCategory(event.target.value);
   }
 
   const addCategory = () => {
-
+    setCategories([...categories, newCategory]);
+    setNewCategory("");
   }
 
-
-
-  const handleSubmit = () => {
-
+  const handleSubmit = (values: IFormSkill) => {
+    notification['success']({
+      message: 'Success',
+      description:
+        id ? 'Edited skill' : `Skill ${values.skillName} has been successfully saved`,
+    });
     history.push("/skill-list");
   }
 
@@ -62,35 +77,42 @@ export const SkillForm: React.FC<ISkillForm> = ({id}) => {
       { (props) => {
         return (
           <Form className={styles.Form}>
-            <FormItem name="name" label="Name" className={styles.Label}>
-              <Input name="name" placeholder="name"></Input>
+            <FormItem name="skillName" >
+              <label htmlFor="firstName" className={styles.Label}>Name</label>
+              <Input id="skillName" name="skillName" placeholder="name"></Input>
             </FormItem>
-            <FormItem name="category" label="Category" className={styles.Label}>
-              <Select name="category" >
+            <FormItem name="skillCategory" >
+              <label htmlFor="skillCategory" className={styles.Label}>Category</label>
+              <Select id="skillCategory" name="skillCategory" dropdownRender={(menu: any) => (
+                <div>
+                  {menu}
+                  <Divider style={{ margin: '4px 0' }} />
+                  <div style={{ display: 'flex', flexWrap: 'nowrap', padding: 8 }}>
+                    <InputAnt style={{ flex: 'auto' }}  onChange={onInputAddCategoryChange} value={newCategory}/>
+                    <a
+                      style={{ flex: 'none', padding: '8px', display: 'block', cursor: 'pointer' }}
+                      onClick={addCategory}
+                    >
+                      <PlusOutlined /> Add item
+                    </a>
+                  </div>
+                </div>
+              )}>
                 {categories.map((category, index) => (
                   <Select.Option key={index} value={category}>
                     {category}
                   </Select.Option>
                 ))}
-                <Divider style={{ margin: '4px 0' }} />
-                <div style={{ display: 'flex', flexWrap: 'nowrap', padding: 8 }}>
-                  <Input style={{ flex: 'auto' }}  name="addCategory" />
-                  <a
-                    style={{ flex: 'none', padding: '8px', display: 'block', cursor: 'pointer' }}
-                    onClick={addCategory}
-                  >
-                    <PlusOutlined /> Add item
-                  </a>
-                </div>
               </Select>
             </FormItem>
-            <FormItem name="skillWeight" label="Skill weight" className={styles.Label}>
-              <Select name="skillWeight">
-                {/*{Array.from(seniorityMap.keys()).map((key) => (*/}
-                {/*  <Select.Option key={key} value={key}>*/}
-                {/*    {seniorityMap.get(key)}*/}
-                {/*  </Select.Option>*/}
-                {/*))}*/}
+            <FormItem name="skillWeight" >
+              <label htmlFor="skillWeight" className={styles.Label}>Skill weight</label>
+              <Select id="skillWeight" name="skillWeight">
+                {Array.from(skillWeightMap.keys()).map((key) => (
+                  <Select.Option key={key} value={key}>
+                    {skillWeightMap.get(key)}
+                  </Select.Option>
+                ))}
               </Select>
             </FormItem>
             <SubmitButton onClick={()=> !submitted && setSubmitted(true)}>Submit</SubmitButton >
@@ -99,4 +121,8 @@ export const SkillForm: React.FC<ISkillForm> = ({id}) => {
       }}
     </Formik>
   )
+}
+
+function isInstanceOfISkill (object: any): object is ISkill {
+  return Object.values(SkillWeight).includes(object.skillWeight); //write category
 }
