88430b0a07f9821944cfb75109645bb1a37afa40
Daniel Majewski <danielmajewski82@gmail.com>
2020-09-15 12:25:40 +0200

    [wip]skill view fixed

--- src/pages/SkillList/SkillListPage.tsx
+++ src/pages/SkillList/SkillListPage.tsx
@@ -1,80 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./SkillListPage.module.scss";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
 import { SkillList } from "../../components/SkillList/SkillList";
 import { ISkill, SkillWeight } from "../../models/employee";
-import { SkillsSvc } from "../../services/SkillsSvc";
+import { SkillsSvc } from "../../services/EmployeesSvc";
 
-const skills: ISkill[] = [
-  {
-    skillName: "Word",
-    skillCategory: "Programming languages",
-    skillWeight: SkillWeight["3/5"],
-  },
-  {
-    skillName: "Excel",
-    skillCategory: "Programming languages",
-    skillWeight: SkillWeight["4/5"],
-  },
-  {
-    skillName: "Java",
-    skillCategory: "Programming languages",
-    skillWeight: SkillWeight["2/5"],
-  },
-  {
-    skillName: "Alcohol resistance",
-    skillCategory: "Soft skills",
-    skillWeight: SkillWeight["5/5"],
-  },
-  {
-    skillName: "Windows",
-    skillCategory: "Environment",
-    skillWeight: SkillWeight["4/5"],
-  },
-  {
-    skillName: "Linux",
-    skillCategory: "Environment",
-    skillWeight: SkillWeight["1/5"],
-  },
-  {
-    skillName: "Script",
-    skillCategory: "Programming languages",
-    skillWeight: SkillWeight["2/5"],
-  },
-  {
-    skillName: "Git",
-    skillCategory: "Devops",
-    skillWeight: SkillWeight["3/5"],
-  },
-  {
-    skillName: "Google searching",
-    skillCategory: "Intelligence",
-    skillWeight: SkillWeight["3/5"],
-  },
-  {
-    skillName: "Task delegation",
-    skillCategory: "Intelligence",
-    skillWeight: SkillWeight["1/5"],
-  },
-  {
-    skillName: "Salary negotiation skill",
-    skillCategory: "Soft skills",
-    skillWeight: SkillWeight["1/5"],
-  },
-];
+// const skills: ISkill[] = [
+//   {
+//     skillName: "Word",
+//     skillCategory: "Programming languages",
+//     skillWeight: SkillWeight["3/5"],
+//   },
+//   {
+//     skillName: "Excel",
+//     skillCategory: "Programming languages",
+//     skillWeight: SkillWeight["4/5"],
+//   },
+//   {
+//     skillName: "Java",
+//     skillCategory: "Programming languages",
+//     skillWeight: SkillWeight["2/5"],
+//   },
+//   {
+//     skillName: "Alcohol resistance",
+//     skillCategory: "Soft skills",
+//     skillWeight: SkillWeight["5/5"],
+//   },
+//   {
+//     skillName: "Windows",
+//     skillCategory: "Environment",
+//     skillWeight: SkillWeight["4/5"],
+//   },
+//   {
+//     skillName: "Linux",
+//     skillCategory: "Environment",
+//     skillWeight: SkillWeight["1/5"],
+//   },
+//   {
+//     skillName: "Script",
+//     skillCategory: "Programming languages",
+//     skillWeight: SkillWeight["2/5"],
+//   },
+//   {
+//     skillName: "Git",
+//     skillCategory: "Devops",
+//     skillWeight: SkillWeight["3/5"],
+//   },
+//   {
+//     skillName: "Google searching",
+//     skillCategory: "Intelligence",
+//     skillWeight: SkillWeight["3/5"],
+//   },
+//   {
+//     skillName: "Task delegation",
+//     skillCategory: "Intelligence",
+//     skillWeight: SkillWeight["1/5"],
+//   },
+//   {
+//     skillName: "Salary negotiation skill",
+//     skillCategory: "Soft skills",
+//     skillWeight: SkillWeight["1/5"],
+//   },
+// ];
 
 const SkillListPage: React.FC<ISkill[]> = () => {
-  // const [skills, setSkills] = useState([]);
+  const [skills, setSkills] = useState([]);
 
   const fetchSkills = async () => {
     try {
       const skillData = await SkillsSvc.getSkill();
-      // setSkills(skillData);
+      setSkills(skillData);
     } catch (e) {
       console.log("fetchSkills:: error::", e);
     }
   };
+
+  useEffect(() => {
+    fetchSkills();
+  }, []);
 
   return (
     <>
