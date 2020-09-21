2e2a0a8baf31ce102c6a282223c2dce8ac7f31d9
Daniel Majewski <danielmajewski82@gmail.com>
2020-09-21 08:40:05 +0200

    [WIP]header styled

--- src/components/SkillNameField/SkillNameField.stories.tsx
+++ src/components/SkillNameField/SkillNameField.stories.tsx
@@ -1,28 +1,21 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { SkillNameField, ISkillName } from "./SkillNameField";
-import { ISkill } from "../../models/employee";
+import { SkillNameField } from "./SkillNameField";
 
 export default {
   title: "Storybook/SkillNameField",
   component: SkillNameField,
 } as Meta;
 
-const Template: Story<ISkillName> = (skillName) => (
+const Template: Story<String> = (skillName) => (
   <SkillNameField {...skillName} />
 );
 
 export const ExampleSkillName1Field = Template.bind({});
-ExampleSkillName1Field.args = {
-  skillName: "Word",
-};
+ExampleSkillName1Field.args = "Word";
 
 export const ExampleSkillName2Field = Template.bind({});
-ExampleSkillName2Field.args = {
-  skillName: "JavaScript",
-};
+ExampleSkillName2Field.args = "JavaScript";
 
 export const ExampleSkillName3Field = Template.bind({});
-ExampleSkillName3Field.args = {
-  skillName: "Alcohol resistance",
-};
+ExampleSkillName3Field.args = "Alcohol resistance";
