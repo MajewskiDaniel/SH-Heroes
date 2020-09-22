301e2dfa62380d17410fe0531253ad94e17565a0
Daniel Majewski <danielmajewski82@gmail.com>
2020-09-22 15:13:40 +0200

    [DONE]new method to edit skillLevel added

--- api/employees/routes/index.ts
+++ api/employees/routes/index.ts
@@ -1,11 +1,11 @@
-import {Request, Response} from "express";
-import {EmployeesService} from "../services";
+import { Request, Response } from "express";
+import { EmployeesService } from "../services";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const employeesService = new EmployeesService();
 
-router.get('/', async (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
   try {
     const employees = await employeesService.getEmployees(req.params);
     res.status(200).send(employees);
@@ -14,7 +14,7 @@ router.get('/', async (req: Request, res: Response) => {
   }
 });
 
-router.get('/:id', async (req: Request, res: Response) => {
+router.get("/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const employee = await employeesService.getEmployeeById(id);
@@ -24,20 +24,20 @@ router.get('/:id', async (req: Request, res: Response) => {
   }
 });
 
-router.post('/', async (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
   try {
     const employee = await employeesService.addEmployee(req.body);
     res.status(201).send(employee);
-  } catch(e) {
-    if(e.message = "occurred") {
-      res.status(409).send('error: user already exists')
+  } catch (e) {
+    if (e.message === "occurred") {
+      res.status(409).send("error: user already exists");
     } else {
       res.status(418).send(`error: can't post user`);
     }
   }
 });
 
-router.put('/:id', async (req: Request, res: Response) => {
+router.put("/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     await employeesService.editEmployee(id, req.body);
@@ -47,7 +47,21 @@ router.put('/:id', async (req: Request, res: Response) => {
   }
 });
 
-router.delete('/:id', async (req: Request, res: Response) => {
+router.patch("/:idE/skills/:idS", async (req: Request, res: Response) => {
+  const { idE, idS } = req.params;
+  const { skillLevel } = req.body;
+
+  try {
+    await employeesService.updateSkill(idE, idS, skillLevel);
+    res
+      .status(200)
+      .send(`skill nr: ${idS} updated to ${skillLevel} in employee nr: ${idE}`);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     await employeesService.deleteEmployee(id);
@@ -57,6 +71,4 @@ router.delete('/:id', async (req: Request, res: Response) => {
   }
 });
 
-export  {
-  router,
-}
+export { router };
