ea17054790dc5a55ea4e77534ebf30cc6d187478
Daniel Majewski <danielmajewski82@gmail.com>
2020-09-24 12:34:23 +0200

    [DONE]sticked header

--- src/components/TableFirstRow/TableFirstRow.tsx
+++ src/components/TableFirstRow/TableFirstRow.tsx
@@ -1,27 +1,27 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren } from "react";
 import { IDynamic } from "../SkillMatrixTable/SkillMatrixTable";
 
-import styles from './TableFirstRow.module.scss';
+import styles from "./TableFirstRow.module.scss";
 
 export interface ITableFirstRowProps {
-  skillMatch: IDynamic
+  skillMatch: IDynamic;
 }
 
-export const TableFirstRow: React.FC<PropsWithChildren<ITableFirstRowProps>> = ({skillMatch , children}) => {
+export const TableFirstRow: React.FC<PropsWithChildren<
+  ITableFirstRowProps
+>> = ({ skillMatch, children }) => {
   return (
     <tr className={styles.Row}>
-      <th className={styles.Title}><div className={styles.TitleCell}>{children}</div> </th>
-      {
-        Object.keys(skillMatch)?.map((skillValue) => {
-          return (
-            <td className={styles.Cell}>
-              <div className={styles.Container}>
-               {skillMatch[skillValue]}
-              </div>
+      <th className={styles.Title}>
+        <div className={styles.TitleCell}>{children}</div>{" "}
+      </th>
+      {Object.keys(skillMatch)?.map((skillValue) => {
+        return (
+          <td className={styles.Cell}>
+            <div className={styles.Container}>{skillMatch[skillValue]}</div>
           </td>
-          )
-        })
-      }
+        );
+      })}
     </tr>
   );
-}
+};
