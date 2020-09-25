4a1d417ff1c6dee8c707bc80eb6b7c921d6b32ca
AgnieszkaJarosik <jarosikloj@gmail.com>
2020-09-25 12:45:30 +0200

    added logout

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Layout, Input, Button} from 'antd';
 import { MenuUnfoldOutlined, MenuFoldOutlined, } from '@ant-design/icons';
 import { Authentication } from "../../services/Authentication";
+import { Consumer } from "../../components/AuthContext/AuthContext";
 import styles from "./MainHeader.module.scss";
 
 const { Header } = Layout;
@@ -12,12 +13,22 @@ export interface IHeader {
   toggle: ()=>void;
 }
 
-export const MainHeader: React.FC<IHeader> = ({collapsed, toggle}) => (
-  < Header className={styles.Header}>
-    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-      className: 'trigger',
-      onClick: toggle
-    })}
-    <Button className={styles.Button} onClick={()=>Authentication.logout()}>Log out</Button>
-  </Header>
-);
+export const MainHeader: React.FC<IHeader> = ({collapsed, toggle}) => {
+  const logOut = (setAuth: (value: boolean) => void) => {
+    Authentication.logout();
+    setAuth(false);
+  }
+  return (
+    < Header className={styles.Header}>
+      {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+        className: 'trigger',
+        onClick: toggle
+      })}
+      <Consumer>
+        {({setAuth}) => (
+          <Button className={styles.Button} onClick={(value)=>logOut(setAuth)}>Log out</Button>
+        )}
+      </Consumer>
+    </Header>
+  );
+}
